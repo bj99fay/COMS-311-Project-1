136ec2044b675657d26bc6564c736d311a645fd4
Riley Thoma <rthoma@iastate.edu>
2020-03-25 15:30:01 -0500

    intervalSearch implemented, with overlap function added, needs testing

--- Fay_Thoma_Project1/src/IntervalTreap.java
+++ Fay_Thoma_Project1/src/IntervalTreap.java
@@ -38,8 +38,46 @@ public class IntervalTreap {
 		//TODO
 	}
 	
+	/**
+	 * Searching for an interval in the database that overlaps interval i
+	 * To be implemented in O(log n) time
+	 * @param i
+	 * 	the given interval 
+	 * @return
+	 * 	Success: an interval that overlaps with i
+	 * 	Unsuccessful: return null
+	 */
 	public Node intervalSearch(Interval i) {
-		//TODO
+		// IntervalSearch from CLRS
+		Node x = root;
+		while(x != null && !intervalOverlap(i, x.getInterv())) {
+			if(x.getLeft() != null && x.getLeft().getIMax() >= i.getLow()) {
+				x = x.getLeft();
+			}
+			else {
+				x = x.getRight();
+			}
+		}
+		return x;
+	}
+	
+	/**
+	 * Check if intervals i and x overlap using CLRS situations outlined
+	 * @param i
+	 * 	interval i
+	 * @param x
+	 * 	interval x
+	 * @return
+	 * 	true if intervals overlap
+	 *  false if intervals do not overlap
+	 */
+	public boolean intervalOverlap(Interval i, Interval x) {
+		boolean overlap = false;
+		//check if they overlap using situations given by CLRS
+		if(i.getLow() <= x.getHigh() && x.getLow() <= i.getHigh()) {
+			overlap = true;
+		}
+		return overlap;
 	}
 	
 	/**
@@ -50,6 +88,7 @@ public class IntervalTreap {
 	 */
 	public Node intervalSearchExactly(Interval i) {
 		//TODO
+		return null;
 	}
 	
 	/**
@@ -60,6 +99,7 @@ public class IntervalTreap {
 	 */
 	public List<Interval> overlappingIntervals(Interval i) {
 		//TODO
+		return null;
 	}
 	
 }
